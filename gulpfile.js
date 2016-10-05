a98cd20b62273491fb004c78b5453c01706398ce
Sean Segal <Sean_Segal@brown.edu>
2016-10-04 23:10:29 -0400

    Events moved to data

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
+var data = require('gulp-data');
 var nunjucksRender = require('gulp-nunjucks-render');
 
 
 gulp.task('default', function() {
 	return gulp.src('pages/*.nunjucks') 
+	.pipe(data(function() {
+      return require('./data/data.json');
+    }))
 	.pipe(nunjucksRender({
 	path: ['pages/', 'templates/'] // String or Array
 	}))
