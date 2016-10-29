4f9f1127d5f5682e75fb9cdeda0c1e5cee220b5d
Sean Segal <Sean_Segal@brown.edu>
2016-10-29 13:05:20 -0400

    Added press tab

--- util/csv-to-json.py
+++ util/csv-to-json.py
@@ -6,12 +6,19 @@ import json
 if len(sys.argv) != 2:
 	print 'Please input a CSV file.'
 
+press = []
 with open(sys.argv[1], 'rb') as csvfile:
 	rows = csv.reader(csvfile, delimiter=',', quotechar='|')
 	for row in rows:
-		print row
+		if len(row) == 2:		
+			press_event = {}
+			press_event['title'] = row[0]
+			press_event['link'] = row[1]
+			press.append(press_event)
+		else:
+			'Found row of incorrect size'
 
 f = sys.argv[1]
 
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile)
+with open('data-test.json', 'w') as outfile:
+    json.dump(press, outfile)
