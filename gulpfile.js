7f459b4cfa42668ca64f029995f4c50b4bb3f512
Yusuf Karim <ykarim250@gmail.com>
2019-01-12 21:12:43 -0500

    🔧 Gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
 var data = require('gulp-data');
 var nunjucksRender = require('gulp-nunjucks-render');
+var sass = require('gulp-sass');
 
+sass.compiler = require('node-sass');
 
 gulp.task('default', function() {
-	return gulp.src('src/pages/*.nunjucks')
+	return gulp.src('./src/scss/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./css'))
+	.pipe(gulp.src('src/pages/*.nunjucks'))
 	.pipe(data(function() {
       return require('./src/data/data.json');
     }))
@@ -12,4 +17,14 @@ gulp.task('default', function() {
 	path: ['src/pages/', 'src/templates/'] // String or Array
 	}))
 	.pipe(gulp.dest('.'));
+});
+
+gulp.task('sass', function () {
+	return gulp.src('./src/scss/*.scss')
+	  .pipe(sass().on('error', sass.logError))
+	  .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./src/scss/*.scss', ['sass']);
 });
