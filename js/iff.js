d01b7394ebccb964037dfb62d684d0f53f4cae4f
Katie Friis <katiefriis@gmail.com>
2019-03-23 19:15:56 -0400

    lightbox

--- js/iff.js
+++ js/iff.js
@@ -31,6 +31,27 @@ $('#newsletter-button').click(function(){
 
 });
 
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('modal-image');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
 /* Scrolling */
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
